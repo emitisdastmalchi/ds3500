b24e6930877dfab002b9fbfd8a2c0bcc16e0f236
emitisdastmalchi <emithind@gmail.com>
2026-09-18 14:00:28 -0400

    gitignore

---
+++ class1-exercise.py/exercise1.py
@@ -0,0 +1,7 @@
+echo "# ds3500" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/emitisdastmalchi/ds3500.git
+git push -u origin main
